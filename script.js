3d5558d83859177cc552471c74825882dca7f220
sulaiman001221 <sulaimanndlovu@gmail.com>
2024-03-22 17:20:26 +0200

    initial push

---
+++ script.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function calculate() {
+    var expression = document.getElementById('display').value;
+    var result = eval(expression);
+    document.getElementById('display').value = result;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
